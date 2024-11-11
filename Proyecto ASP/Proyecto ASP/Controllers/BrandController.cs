84b5e40be13a0adb57fd909c32338d7f11eea8f7
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2024-11-11 19:14:59 +0100

    Pruebas

---
+++ Proyecto ASP/Proyecto ASP/Controllers/BrandController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_ASP.Models;
+
+namespace Proyecto_ASP.Controllers
+{
+    public class BrandController : Controller
+    {
+
+        private readonly PubContext _context;
+
+        public BrandController(PubContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Brands.ToListAsync());
+        }
+    }
+}
